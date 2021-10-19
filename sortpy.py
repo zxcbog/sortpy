59bda16d7889b45f0e7b3dc28922654e76b63a31
zxcbog <92804003+zxcbog@users.noreply.github.com>
2021-10-19 21:24:30 +0700

    Create sortpy.py
    универсальная сортировка по значениям таблицы ascii + подсчет одинаковых символов

---
+++ sortpy.py
@@ -0,0 +1,30 @@
+c = 'хуцйхсыфвцйзху-3219гу8цйовлдыфовлджцйо342'
+c = list(c)
+g = [1]
+e = 0
+running = True
+pr = False
+i = 0
+while running and pr == False:
+    if ord(c[i+1]) < ord(c[i]):
+        c[i], c[i+1] = c[i + 1], c[i]
+    i += 1
+    if i == len(c) - 1:
+        for j in range(len(c) - 1):
+            if c[j] > c[j + 1]:
+                i = 0
+                pr = False
+                break
+            else:
+                pr = True
+for j in range(len(c) - 1):
+    if (c[j] == c[j + 1]):
+        g[e] += 1
+    else:
+        g += [1]
+        e += 1
+print(c)
+i = 0
+for es in range(e + 1):
+    i += g[es]
+    print((c[i - 1]),':', g[es])
