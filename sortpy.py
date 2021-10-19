990bd3e714a9d548801e4a1df1b4717116bb3562
zxcbog <92804003+zxcbog@users.noreply.github.com>
2021-10-19 21:53:23 +0700

    Update sortpy.py

--- sortpy.py
+++ sortpy.py
@@ -1,29 +1,33 @@
-c = 'text'
+c = str(input())
 c = list(c)
-g = [1]
-e = 0
-running = True
-pr = False
-i = 0
-while running and pr == False:
-    if ord(c[i+1]) < ord(c[i]):
-        c[i], c[i+1] = c[i + 1], c[i]
-    i += 1
-    if i == len(c) - 1:
-        for j in range(len(c) - 1):
-            if c[j] > c[j + 1]:
-                i = 0
-                pr = False
-                break
-            else:
-                pr = True
-for j in range(len(c) - 1):
-    if (c[j] == c[j + 1]):
-        g[e] += 1
-    else:
-        g += [1]
-        e += 1
-print(c)
+def sort(c):
+    running = True
+    pr = False
+    g = [1]
+    e = 0
+    i = 0
+    while running and pr == False:
+        if ord(c[i+1]) < ord(c[i]):
+            c[i], c[i+1] = c[i + 1], c[i]
+        i += 1
+        if i == len(c) - 1:
+            for j in range(len(c) - 1):
+                if c[j] > c[j + 1]:
+                    i = 0
+                    pr = False
+                    break
+                else:
+                    pr = True
+    for j in range(len(c) - 1):
+        if (c[j] == c[j + 1]):
+            g[e] += 1
+        else:
+            g += [1]
+            e += 1
+    return c, g, e
+c1 = sort(c)[0]
+g, e = sort(c)[1], sort(c)[2]
+print(sort(c)[0])
 i = 0
 for es in range(e + 1):
     i += g[es]
